9b37068106ab5125405b6455f878bc45ba4ed95f
Vl_qs <99005319+Vlqs1@users.noreply.github.com>
2022-02-04 14:15:57 -0500

    Just A GUI V2

--- Just A GUI V2.lua
+++ Just A GUI V2.lua
@@ -1,7 +1,8 @@
--- Made By Vl_qs (Credits To All The Original Owners Of The Scripts)
+-- Made By Vl_qs (Credits to all original owners of the scripts)
 
-local JustAGUIV1 = Instance.new("ScreenGui")
+local ScreenGui = Instance.new("ScreenGui")
 local main = Instance.new("Frame")
+local Frame = Instance.new("Frame")
 local TextLabel = Instance.new("TextLabel")
 local DarkHub = Instance.new("TextButton")
 local Parkour = Instance.new("TextButton")
@@ -10,39 +11,50 @@ local AUT = Instance.new("TextButton")
 local OwlHub = Instance.new("TextButton")
 local MM2 = Instance.new("TextButton")
 local BP2 = Instance.new("TextButton")
-local Kat = Instance.new("TextButton")
+local KAT = Instance.new("TextButton")
 local Yield = Instance.new("TextButton")
 
 
 
-JustAGUIV1.Name = "Just A GUI V1"
-JustAGUIV1.Parent = game.CoreGui
-JustAGUIV1.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 main.Name = "main"
-main.Parent = JustAGUIV1
-main.BackgroundColor3 = Color3.fromRGB(77, 77, 77)
-main.Position = UDim2.new(0.281798244, 0, 0.200642049, 0)
-main.Size = UDim2.new(0, 427, 0, 441)
+main.Parent = ScreenGui
+main.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+main.BackgroundTransparency = 0.200
+main.BorderColor3 = Color3.fromRGB(255, 255, 255)
+main.Position = UDim2.new(0.319078952, 0, 0.205457464, 0)
+main.Size = UDim2.new(0, 350, 0, 387)
 main.Active = true
 main.Draggable = true
 
-TextLabel.Parent = JustAGUIV1
-TextLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.Position = UDim2.new(0.281798244, 0, 0.200642049, 0)
-TextLabel.Size = UDim2.new(0, 427, 0, 50)
+Frame.Parent = main
+Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BackgroundTransparency = 0.200
+Frame.BorderColor3 = Color3.fromRGB(255, 255, 255)
+Frame.Position = UDim2.new(0.0221365988, 0, 0.0177270025, 0)
+Frame.Size = UDim2.new(0, 333, 0, 38)
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderColor3 = Color3.fromRGB(27, 42, 53)
+TextLabel.Position = UDim2.new(-0.00524535775, 0, -0.0313847251, 0)
+TextLabel.Size = UDim2.new(0, 334, 0, 40)
 TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "Just A GUI V1 | By Vl_qs"
+TextLabel.Text = "Just A GUI V2 | Vl_qs"
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextScaled = true
 TextLabel.TextSize = 14.000
 TextLabel.TextWrapped = true
 
 DarkHub.Name = "DarkHub"
-DarkHub.Parent = JustAGUIV1
+DarkHub.Parent = main
 DarkHub.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-DarkHub.Position = UDim2.new(0.286184222, 0, 0.332263231, 0)
-DarkHub.Size = UDim2.new(0, 200, 0, 50)
+DarkHub.BorderColor3 = Color3.fromRGB(255, 255, 255)
+DarkHub.Position = UDim2.new(0.0514285713, 0, 0.152454779, 0)
+DarkHub.Size = UDim2.new(0, 127, 0, 50)
 DarkHub.Font = Enum.Font.SourceSans
 DarkHub.Text = "Dark Hub"
 DarkHub.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -54,12 +66,13 @@ DarkHub.MouseButton1Down:connect(function()
 end)
 
 Parkour.Name = "Parkour"
-Parkour.Parent = JustAGUIV1
+Parkour.Parent = main
 Parkour.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Parkour.Position = UDim2.new(0.286184222, 0, 0.457463861, 0)
-Parkour.Size = UDim2.new(0, 200, 0, 50)
+Parkour.BorderColor3 = Color3.fromRGB(255, 255, 255)
+Parkour.Position = UDim2.new(0.0514285713, 0, 0.333333313, 0)
+Parkour.Size = UDim2.new(0, 127, 0, 50)
 Parkour.Font = Enum.Font.SourceSans
-Parkour.Text = "Parkour"
+Parkour.Text = "Parkour GUI"
 Parkour.TextColor3 = Color3.fromRGB(255, 255, 255)
 Parkour.TextScaled = true
 Parkour.TextSize = 14.000
@@ -69,10 +82,11 @@ Parkour.MouseButton1Down:connect(function()
 end)
 
 BP.Name = "BP"
-BP.Parent = JustAGUIV1
+BP.Parent = main
 BP.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-BP.Position = UDim2.new(0.286184222, 0, 0.606741548, 0)
-BP.Size = UDim2.new(0, 200, 0, 50)
+BP.BorderColor3 = Color3.fromRGB(255, 255, 255)
+BP.Position = UDim2.new(0.0514285713, 0, 0.524547815, 0)
+BP.Size = UDim2.new(0, 127, 0, 50)
 BP.Font = Enum.Font.SourceSans
 BP.Text = "BP GUI (only works in bp)"
 BP.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -84,12 +98,13 @@ BP.MouseButton1Down:connect(function()
 end)
 
 AUT.Name = "AUT"
-AUT.Parent = JustAGUIV1
+AUT.Parent = main
 AUT.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-AUT.Position = UDim2.new(0.286184222, 0, 0.751203835, 0)
-AUT.Size = UDim2.new(0, 200, 0, 50)
+AUT.BorderColor3 = Color3.fromRGB(255, 255, 255)
+AUT.Position = UDim2.new(0.0514285713, 0, 0.720930219, 0)
+AUT.Size = UDim2.new(0, 127, 0, 50)
 AUT.Font = Enum.Font.SourceSans
-AUT.Text = "AUT Gui"
+AUT.Text = "AUT GUI"
 AUT.TextColor3 = Color3.fromRGB(255, 255, 255)
 AUT.TextScaled = true
 AUT.TextSize = 14.000
@@ -99,10 +114,11 @@ AUT.MouseButton1Down:connect(function()
 end)
 
 OwlHub.Name = "OwlHub"
-OwlHub.Parent = JustAGUIV1
+OwlHub.Parent = main
 OwlHub.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-OwlHub.Position = UDim2.new(0.46084851, 0, 0.332263231, 0)
-OwlHub.Size = UDim2.new(0, 200, 0, 50)
+OwlHub.BorderColor3 = Color3.fromRGB(255, 255, 255)
+OwlHub.Position = UDim2.new(0.557142854, 0, 0.152454793, 0)
+OwlHub.Size = UDim2.new(0, 127, 0, 50)
 OwlHub.Font = Enum.Font.SourceSans
 OwlHub.Text = "Owl Hub"
 OwlHub.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -114,12 +130,13 @@ OwlHub.MouseButton1Down:connect(function()
 end)
 
 MM2.Name = "MM2"
-MM2.Parent = JustAGUIV1
+MM2.Parent = main
 MM2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-MM2.Position = UDim2.new(0.46084857, 0, 0.457463861, 0)
-MM2.Size = UDim2.new(0, 200, 0, 50)
+MM2.BorderColor3 = Color3.fromRGB(255, 255, 255)
+MM2.Position = UDim2.new(0.557142854, 0, 0.333333343, 0)
+MM2.Size = UDim2.new(0, 127, 0, 50)
 MM2.Font = Enum.Font.SourceSans
-MM2.Text = "MM2"
+MM2.Text = "MM2 GUI"
 MM2.TextColor3 = Color3.fromRGB(255, 255, 255)
 MM2.TextScaled = true
 MM2.TextSize = 14.000
@@ -129,12 +146,13 @@ MM2.MouseButton1Down:connect(function()
 end)
 
 BP2.Name = "BP2"
-BP2.Parent = JustAGUIV1
+BP2.Parent = main
 BP2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-BP2.Position = UDim2.new(0.46084857, 0, 0.606741548, 0)
-BP2.Size = UDim2.new(0, 200, 0, 50)
+BP2.BorderColor3 = Color3.fromRGB(255, 255, 255)
+BP2.Position = UDim2.new(0.557142854, 0, 0.524547815, 0)
+BP2.Size = UDim2.new(0, 127, 0, 50)
 BP2.Font = Enum.Font.SourceSans
-BP2.Text = "BP credits (Only works in all gamemodes, wait for bp gamemode and wait for your turn to use the gun.)"
+BP2.Text = "BP credits (click this once you have the gun in bp gamemode)"
 BP2.TextColor3 = Color3.fromRGB(255, 255, 255)
 BP2.TextScaled = true
 BP2.TextSize = 14.000
@@ -151,26 +169,28 @@ BP2.MouseButton1Down:connect(function()
 	end
 end)
 
-Kat.Name = "Kat"
-Kat.Parent = JustAGUIV1
-Kat.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Kat.Position = UDim2.new(0.46084851, 0, 0.751203835, 0)
-Kat.Size = UDim2.new(0, 200, 0, 50)
-Kat.Font = Enum.Font.SourceSans
-Kat.Text = "KAT Gui"
-Kat.TextColor3 = Color3.fromRGB(255, 255, 255)
-Kat.TextScaled = true
-Kat.TextSize = 14.000
-Kat.TextWrapped = true
-Kat.MouseButton1Down:connect(function()
+KAT.Name = "KAT"
+KAT.Parent = main
+KAT.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+KAT.BorderColor3 = Color3.fromRGB(255, 255, 255)
+KAT.Position = UDim2.new(0.557142854, 0, 0.720930219, 0)
+KAT.Size = UDim2.new(0, 127, 0, 50)
+KAT.Font = Enum.Font.SourceSans
+KAT.Text = "KAT GUI"
+KAT.TextColor3 = Color3.fromRGB(255, 255, 255)
+KAT.TextScaled = true
+KAT.TextSize = 14.000
+KAT.TextWrapped = true
+KAT.MouseButton1Down:connect(function()
 	loadstring(game:HttpGet(('https://raw.githubusercontent.com/mememasterboi/a-lot-of-scripts/master/Output%20(6).lua'),true))()
 end)
 
 Yield.Name = "Yield"
-Yield.Parent = JustAGUIV1
+Yield.Parent = main
 Yield.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Yield.Position = UDim2.new(0.371248037, 0, 0.845906913, 0)
-Yield.Size = UDim2.new(0, 200, 0, 31)
+Yield.BorderColor3 = Color3.fromRGB(255, 255, 255)
+Yield.Position = UDim2.new(0.28285715, 0, 0.868217051, 0)
+Yield.Size = UDim2.new(0, 127, 0, 35)
 Yield.Font = Enum.Font.SourceSans
 Yield.Text = "EXTRA | INF YIELD"
 Yield.TextColor3 = Color3.fromRGB(255, 255, 255)
