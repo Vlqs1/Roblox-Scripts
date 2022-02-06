e99c317e1e259efee9b1423579f0b9c2a1e0002e
Vl_qs <99005319+Vlqs1@users.noreply.github.com>
2022-02-06 15:23:34 -0500

    Dupe GUI.lua

--- Dupe GUI.lua
+++ Dupe GUI.lua
@@ -1,15 +1,15 @@
 -- Made By Vl_qs (Credits to original owner of the scripts)
-	
+
 local ScreenGui = Instance.new("ScreenGui")
 local main = Instance.new("Frame")
 local Frame = Instance.new("Frame")
 local Frame_2 = Instance.new("Frame")
 local TextLabel = Instance.new("TextLabel")
-local rain = Instance.new("TextButton")
-local Antlers = Instance.new("TextButton")
-local CandyCrown = Instance.new("TextButton")
-local RoyalRed = Instance.new("TextButton")
 local TextLabel_2 = Instance.new("TextLabel")
+local rain = Instance.new("TextButton")
+local RoyalRed = Instance.new("TextButton")
+local CandyCrown = Instance.new("TextButton")
+local Antlers = Instance.new("TextButton")
 
 
 ScreenGui.Parent = game.CoreGui
@@ -18,7 +18,8 @@ ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 main.Name = "main"
 main.Parent = ScreenGui
 main.BackgroundColor3 = Color3.fromRGB(76, 76, 76)
-main.Position = UDim2.new(0.457345963, 0, 0.126829267, 0)
+main.LayoutOrder = 4
+main.Position = UDim2.new(0.474723518, 0, 0.149593502, 0)
 main.Size = UDim2.new(0, 408, 0, 430)
 main.Active = true
 main.Draggable = true
@@ -28,32 +29,44 @@ Frame.BackgroundColor3 = Color3.fromRGB(121, 121, 121)
 Frame.BackgroundTransparency = 0.500
 Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BorderSizePixel = 10
-Frame.Position = UDim2.new(0.0167967677, 0, -0.00107771158, 0)
-Frame.Size = UDim2.new(0, 394, 0, 430)
+Frame.LayoutOrder = 1
+Frame.Position = UDim2.new(0.0121713281, 0, -0.00196635723, 0)
+Frame.Size = UDim2.new(0, 398, 0, 431)
 
-Frame_2.Parent = ScreenGui
+Frame_2.Parent = main
 Frame_2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Frame_2.BackgroundTransparency = 0.500
-Frame_2.Position = UDim2.new(0.531595588, 0, 0.107317075, 0)
+Frame_2.BackgroundTransparency = 1.000
+Frame_2.Position = UDim2.new(0.237477943, 0, -0.0950085074, 0)
 Frame_2.Size = UDim2.new(0, 210, 0, 36)
 
-TextLabel.Parent = ScreenGui
+TextLabel.Parent = main
 TextLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.Position = UDim2.new(0.531595588, 0, 0.0373983756, 0)
-TextLabel.Size = UDim2.new(0, 210, 0, 43)
+TextLabel.Position = UDim2.new(0.0135558844, 0, 0.796445429, 0)
+TextLabel.Size = UDim2.new(0, 394, 0, 52)
 TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "Dupe GUI | Vl_qs"
+TextLabel.Text = "Everything requires 10k credits, except /e rain"
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextScaled = true
 TextLabel.TextSize = 14.000
 TextLabel.TextWrapped = true
 
+TextLabel_2.Parent = main
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.Position = UDim2.new(0.237477928, 0, -0.102136515, 0)
+TextLabel_2.Size = UDim2.new(0, 210, 0, 43)
+TextLabel_2.Font = Enum.Font.SourceSans
+TextLabel_2.Text = "Dupe GUI | Vl_qs"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextScaled = true
+TextLabel_2.TextSize = 14.000
+TextLabel_2.TextWrapped = true
+
 rain.Name = "rain"
-rain.Parent = ScreenGui
+rain.Parent = main
 rain.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 rain.BorderColor3 = Color3.fromRGB(255, 255, 255)
 rain.BorderSizePixel = 2
-rain.Position = UDim2.new(0.472353876, 0, 0.209756047, 0)
+rain.Position = UDim2.new(0.043432299, 0, 0.193476975, 0)
 rain.Size = UDim2.new(0, 124, 0, 50)
 rain.Font = Enum.Font.SourceSans
 rain.Text = "/e rain"
@@ -71,59 +84,12 @@ rain.MouseButton1Down:connect(function()
 	game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer(unpack(args))
 end)
 
-Antlers.Name = "Antlers"
-Antlers.Parent = ScreenGui
-Antlers.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Antlers.BorderColor3 = Color3.fromRGB(255, 255, 255)
-Antlers.BorderSizePixel = 2
-Antlers.Position = UDim2.new(0.472353876, 0, 0.484552771, 0)
-Antlers.Size = UDim2.new(0, 124, 0, 50)
-Antlers.Font = Enum.Font.SourceSans
-Antlers.Text = "Neon Antlers"
-Antlers.TextColor3 = Color3.fromRGB(255, 255, 255)
-Antlers.TextScaled = true
-Antlers.TextSize = 14.000
-Antlers.TextWrapped = true
-Antlers.MouseButton1Down:connect(function()
-	local args = {
-		[1] = 66,
-		[2] = "Accessories",
-		[3] = "Knife Antlers"
-	}
-
-	game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer(unpack(args))
-end)
-
-CandyCrown.Name = "Candy Crown"
-CandyCrown.Parent = ScreenGui
-CandyCrown.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-CandyCrown.BorderColor3 = Color3.fromRGB(255, 255, 255)
-CandyCrown.BorderSizePixel = 2
-CandyCrown.Position = UDim2.new(0.655608237, 0, 0.209756032, 0)
-CandyCrown.Size = UDim2.new(0, 124, 0, 50)
-CandyCrown.Font = Enum.Font.SourceSans
-CandyCrown.Text = "Candy Crown"
-CandyCrown.TextColor3 = Color3.fromRGB(255, 255, 255)
-CandyCrown.TextScaled = true
-CandyCrown.TextSize = 14.000
-CandyCrown.TextWrapped = true
-CandyCrown.MouseButton1Down:connect(function()
-	local args = {
-		[1] = 66,
-		[2] = "Knife Skins",
-		[3] = "Winter Gift"
-	}
-
-	game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer(unpack(args))
-
-end)
-
 RoyalRed.Name = "Royal Red"
-RoyalRed.Parent = ScreenGui
+RoyalRed.Parent = main
 RoyalRed.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 RoyalRed.BorderColor3 = Color3.fromRGB(255, 255, 255)
 RoyalRed.BorderSizePixel = 2
-RoyalRed.Position = UDim2.new(0.655608237, 0, 0.484552771, 0)
+RoyalRed.Position = UDim2.new(0.660510182, 0, 0.575250506, 0)
 RoyalRed.Size = UDim2.new(0, 124, 0, 50)
 RoyalRed.Font = Enum.Font.SourceSans
 RoyalRed.Text = "Royal Red"
@@ -141,13 +107,48 @@ RoyalRed.MouseButton1Down:connect(function()
 	game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer(unpack(args))
 end)
 
-TextLabel_2.Parent = ScreenGui
-TextLabel_2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel_2.Position = UDim2.new(0.462085307, 0, 0.656910539, 0)
-TextLabel_2.Size = UDim2.new(0, 394, 0, 52)
-TextLabel_2.Font = Enum.Font.SourceSans
-TextLabel_2.Text = "Everything is 10k credits, except /e rain"
-TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel_2.TextScaled = true
-TextLabel_2.TextSize = 14.000
-TextLabel_2.TextWrapped = true
+CandyCrown.Name = "Candy Crown"
+CandyCrown.Parent = main
+CandyCrown.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+CandyCrown.BorderColor3 = Color3.fromRGB(255, 255, 255)
+CandyCrown.BorderSizePixel = 2
+CandyCrown.Position = UDim2.new(0.660510182, 0, 0.19347696, 0)
+CandyCrown.Size = UDim2.new(0, 124, 0, 50)
+CandyCrown.Font = Enum.Font.SourceSans
+CandyCrown.Text = "Candy Crown"
+CandyCrown.TextColor3 = Color3.fromRGB(255, 255, 255)
+CandyCrown.TextScaled = true
+CandyCrown.TextSize = 14.000
+CandyCrown.TextWrapped = true
+CandyCrown.MouseButton1Down:connect(function()
+	local args = {
+		[1] = 66,
+		[2] = "Knife Skins",
+		[3] = "Winter Gift"
+	}
+
+	game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer(unpack(args))
+end)
+
+Antlers.Name = "Antlers"
+Antlers.Parent = main
+Antlers.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Antlers.BorderColor3 = Color3.fromRGB(255, 255, 255)
+Antlers.BorderSizePixel = 2
+Antlers.Position = UDim2.new(0.043432299, 0, 0.575250506, 0)
+Antlers.Size = UDim2.new(0, 124, 0, 50)
+Antlers.Font = Enum.Font.SourceSans
+Antlers.Text = "Neon Antlers"
+Antlers.TextColor3 = Color3.fromRGB(255, 255, 255)
+Antlers.TextScaled = true
+Antlers.TextSize = 14.000
+Antlers.TextWrapped = true
+Antlers.MouseButton1Down:connect(function()
+	local args = {
+		[1] = 66,
+		[2] = "Accessories",
+		[3] = "Knife Antlers"
+	}
+
+	game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer(unpack(args))
+end)
